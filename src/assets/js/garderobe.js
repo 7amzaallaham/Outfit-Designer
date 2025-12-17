e1ecafa028d0a68ae07a47be60a8175a50783561
Madina <madeena.sharifi109@icloud.com>
2025-12-17 16:13:00 +0100

    "..."button update , tshirts, hosen, shuhe update

--- src/assets/js/garderobe.js
+++ src/assets/js/garderobe.js
@@ -1,26 +1,26 @@
-function filterPopupToggle() {
+function filterPopup() {
     document.getElementById("filterPopup").classList.toggle("aktiv");
 }
 function filterAnwenden() {
     document.getElementById("filterPopup").classList.remove("aktiv");
 }
 
-document.getElementById("filterButton").addEventListener("click", filterPopupToggle);
+document.getElementById("filterButton").addEventListener("click", filterPopup);
 document.getElementById("filterAnwendenButton").addEventListener("click", filterAnwenden);
 //-----------------
 
-function addPopupToggle() {
+function addPopup() {
     document.getElementById("addPopup").classList.toggle("aktiv");
 }
 function hochladen() {
   document.getElementById("addPopup").classList.remove("aktiv");
 }
 
-document.getElementById("addButton").addEventListener("click", addPopupToggle);
+document.getElementById("addButton").addEventListener("click", addPopup);
 document.getElementById("hochladenButton").addEventListener("click", hochladen);
 //-----------------
 
-function farbePopupToggle() {
+function farbePopup() {
     document.getElementById("farbePopup").classList.toggle("aktiv");
 }
 
@@ -28,5 +28,22 @@ function farbeAnwenden() {
     document.getElementById("farbePopup").classList.remove("aktiv");
 }
 
-document.getElementById("farbeAuwahlenButton").addEventListener("click", farbePopupToggle);
+document.getElementById("farbeAuwahlenButton").addEventListener("click", farbePopup);
 document.getElementById("farbeAnwenden").addEventListener("click", farbeAnwenden);
+//---------------
+
+document.querySelectorAll(".löschen_veröffentlichen").forEach(button => {
+    button.addEventListener("click", () => {
+        document.getElementById("aktionPopup").classList.toggle("aktiv");
+    });
+});
+
+document.getElementById("loschenButton").addEventListener("click", () => {
+    alert("Teil wurde gelöscht");
+    document.getElementById("aktionPopup").classList.remove("aktiv");
+});
+
+document.getElementById("veroffentlichenButton").addEventListener("click", () => {
+    alert("Teil wurde veröffentlicht");
+    window.location.href = "community-profil.html";
+});
