3667a69692ee8876ff0092e68bfa7df376fdee41
7amzaallaham <hamza.laham@stud.th-luebeck.de>
2025-12-18 04:58:24 +0100

    Update community.js

--- src/assets/js/community.js
+++ src/assets/js/community.js
@@ -1,1 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const likeButtons = document.querySelectorAll(".community_like");
+
+    likeButtons.forEach(button => {
+        button.addEventListener("click", () => {
+
+            let text = button.innerText;        
+            let number = parseInt(text.match(/\d+/)); 
+
+            number++;
+
+            button.innerText = `Like (${number})`;
+        });
+    });
+
+
+    const commentButtons = document.querySelectorAll(".community_comment");
+
+    commentButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            alert("Comment-Funktion kommt später 🙂");
+        });
+    });
+
+});
