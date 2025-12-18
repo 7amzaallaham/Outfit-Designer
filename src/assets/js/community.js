be31a737a376e895da67fa76195d97b64810f096
7amzaallaham <hamza.laham@stud.th-luebeck.de>
2025-12-18 05:03:32 +0100

    Update community.js

--- src/assets/js/community.js
+++ src/assets/js/community.js
@@ -1,26 +1,42 @@
+class CommunityPost {
+
+    constructor(likeButton, commentButton) {
+        this.likeButton = likeButton;
+        this.commentButton = commentButton;
+
+        this.likes = this.extractNumber(likeButton.innerText);
+        this.comments = this.extractNumber(commentButton.innerText);
+
+        this.initEvents();
+    }
+
+    extractNumber(text) {
+        return parseInt(text.match(/\d+/));
+    }
+
+    like() {
+        this.likes++;
+        this.likeButton.innerText = `Like (${this.likes})`;
+        this.likeButton.classList.add("active");
+    }
+
+    comment() {
+        alert("Comment-Funktion kommt später 🙂");
+    }
+
+    initEvents() {
+        this.likeButton.addEventListener("click", () => this.like());
+        this.commentButton.addEventListener("click", () => this.comment());
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const likeButtons = document.querySelectorAll(".community_like");
-
-    likeButtons.forEach(button => {
-        button.addEventListener("click", () => {
-
-            let text = button.innerText;        
-            let number = parseInt(text.match(/\d+/)); 
-
-            number++;
-
-            button.innerText = `Like (${number})`;
-        });
-    });
-
-
     const commentButtons = document.querySelectorAll(".community_comment");
 
-    commentButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            alert("Comment-Funktion kommt später 🙂");
-        });
-    });
+    for (let i = 0; i < likeButtons.length; i++) {
+        new CommunityPost(likeButtons[i], commentButtons[i]);
+    }
 
 });
