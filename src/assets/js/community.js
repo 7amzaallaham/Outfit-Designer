f8a1f95b7b1a54058a2f750a83613b1715d87b11
7amzaallaham <hamza.laham@stud.th-luebeck.de>
2025-12-19 23:24:04 +0100

    Update community.js

--- src/assets/js/community.js
+++ src/assets/js/community.js
@@ -1,1 +1,17 @@
+// LIKE BUTTON
+document.querySelectorAll(".community_like").forEach(button => {
+    button.addEventListener("click", () => {
+        const countSpan = button.querySelector(".like_count");
+        let count = parseInt(countSpan.textContent);
+        countSpan.textContent = count + 1;
+    });
+});
 
+// COMMENT BUTTON
+document.querySelectorAll(".community_comment").forEach(button => {
+    button.addEventListener("click", () => {
+        const card = button.closest(".outfit_karte");
+        const commentBox = card.querySelector(".comment_box");
+        commentBox.classList.toggle("hidden");
+    });
+});
