46e97ff849ca7cfc0424af53c0ea86508f0be3ab
7amzaallaham <hamza.laham@stud.th-luebeck.de>
2026-01-12 14:42:09 +0100

    Update Hose.java

--- BACKEND/src/main/java/BACKEND/Model/Hose.java
+++ BACKEND/src/main/java/BACKEND/Model/Hose.java
@@ -1,9 +1,15 @@
 package BACKEND.Model;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Hose extends Kleidungsstueck {
+
+    protected Hose() {
+        // JPA 
+    }
 
     public Hose(int id, String bildLink, String farbe, String marke) {
         super(id, bildLink, farbe, marke);
     }
-    
 }
