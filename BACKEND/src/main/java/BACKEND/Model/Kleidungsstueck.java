3e9bbdf6c32910ca9b9149e73e4ec70b369afa40
7amzaallaham <hamza.laham@stud.th-luebeck.de>
2026-01-12 14:41:41 +0100

    Update Kleidungsstueck.java

--- BACKEND/src/main/java/BACKEND/Model/Kleidungsstueck.java
+++ BACKEND/src/main/java/BACKEND/Model/Kleidungsstueck.java
@@ -1,11 +1,22 @@
 package BACKEND.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+
 public abstract class Kleidungsstueck {    //aus dem Klassendiagramm
-        
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private int id;                   //alle public weil sie vom Outfit verwendet werden
     private String bildpfad;
     private String farbe;
     private String marke;
+
+    protected Kleidungsstueck() {
+    }
 
     public Kleidungsstueck(int id, String bildLink, String farbe, String marke) {
         this.id = id;
