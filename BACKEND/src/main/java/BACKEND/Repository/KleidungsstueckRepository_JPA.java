8fc3d10a7fd5e866001241722f65345a1d161bdc
7amzaallaham <hamza.laham@stud.th-luebeck.de>
2026-01-12 17:40:19 +0100

    Update KleidungsstueckRepository_JPA.java

--- BACKEND/src/main/java/BACKEND/Repository/KleidungsstueckRepository_JPA.java
+++ BACKEND/src/main/java/BACKEND/Repository/KleidungsstueckRepository_JPA.java
@@ -1,8 +1,12 @@
 package BACKEND.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.context.annotation.Primary;
 import BACKEND.Model.Kleidungsstueck;
 
+@Repository
+@Primary
 public interface KleidungsstueckRepository_JPA
         extends JpaRepository<Kleidungsstueck, Integer> {
 }
