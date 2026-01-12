ba7fb6f5c56a2631a346d255305a5f4446df2510
7amzaallaham <hamza.laham@stud.th-luebeck.de>
2026-01-12 17:42:33 +0100

    Update OutfitRepository_JPA.java

--- BACKEND/src/main/java/BACKEND/Repository/OutfitRepository_JPA.java
+++ BACKEND/src/main/java/BACKEND/Repository/OutfitRepository_JPA.java
@@ -1,8 +1,12 @@
 package BACKEND.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.context.annotation.Primary;
 import BACKEND.Model.Outfit;
 
+@Repository
+@Primary
 public interface OutfitRepository_JPA
         extends JpaRepository<Outfit, Integer> {
 }
