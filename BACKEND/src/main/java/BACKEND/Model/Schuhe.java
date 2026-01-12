8e5faf035268f6c54ea3cee88dcd1b4ccc93bd8e
7amzaallaham <hamza.laham@stud.th-luebeck.de>
2026-01-12 14:42:54 +0100

    Update Schuhe.java

--- BACKEND/src/main/java/BACKEND/Model/Schuhe.java
+++ BACKEND/src/main/java/BACKEND/Model/Schuhe.java
@@ -1,9 +1,15 @@
 package BACKEND.Model;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Schuhe extends Kleidungsstueck {
+
+    protected Schuhe() {
+        // JPA braucht einen No-Args Constructor
+    }
 
     public Schuhe(int id, String bildLink, String farbe, String marke) {
         super(id, bildLink, farbe, marke);
     }
-    
 }
