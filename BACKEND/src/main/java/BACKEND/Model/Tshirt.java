69f10cd668410673e19028d1ff1087b511f10499
7amzaallaham <hamza.laham@stud.th-luebeck.de>
2026-01-12 14:42:34 +0100

    Update Tshirt.java

--- BACKEND/src/main/java/BACKEND/Model/Tshirt.java
+++ BACKEND/src/main/java/BACKEND/Model/Tshirt.java
@@ -1,6 +1,16 @@
 package BACKEND.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+
 public class Tshirt extends Kleidungsstueck {
+
+    protected Tshirt() {
+
+        //JPA
+
+    }
 
     public Tshirt(int id, String bildLink, String farbe, String marke) {
         super(id, bildLink, farbe, marke);
