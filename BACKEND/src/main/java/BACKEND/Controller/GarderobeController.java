16db9203f5bf527b7c53fe12835ac5e126f21883
ririta4 <zidanrita9@gmail.com>
2026-01-14 16:26:49 +0100

    Update GarderobeController.java

--- BACKEND/src/main/java/BACKEND/Controller/GarderobeController.java
+++ BACKEND/src/main/java/BACKEND/Controller/GarderobeController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import BACKEND.Model.Hose;
+import BACKEND.Model.Outfit;
+import BACKEND.Model.Schuhe;
+import BACKEND.Model.Tshirt;
 import BACKEND.Service.GarderobeService;
 
 @RestController
@@ -14,29 +18,32 @@ import BACKEND.Service.GarderobeService;
 @CrossOrigin(origins = "*")
 public class GarderobeController {
 
+    // Service wird benutzt
     private final GarderobeService service;
 
     public GarderobeController(GarderobeService service) {
         this.service = service;
     }
+    
+    // GET /garderobe/outfits
+    // Frontend bekommt ALLE Outfits
+    @GetMapping("/outfits")
+    public List<Outfit> getOutfits() {
+        return service.getOutfits();
+    }
 
     @GetMapping("/tshirts")
-    public List<String> getTshirts() {
+    public List<Tshirt> getTshirts() {
         return service.getTshirts();
     }
 
     @GetMapping("/hosen")
-    public List<String> getHosen() {
+    public List<Hose> getHosen() {
         return service.getHosen();
     }
 
     @GetMapping("/schuhe")
-    public List<String> getSchuhe() {
+    public List<Schuhe> getSchuhe() {
         return service.getSchuhe();
-    }
-
-    @GetMapping("/outfits")
-    public List<List<String>> getOutfits() {
-        return service.getOutfits();
     }
 }
