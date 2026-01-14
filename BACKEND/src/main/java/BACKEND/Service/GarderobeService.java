e13ddf468a045cc89df76539e6491cb5ff91558d
ririta4 <zidanrita9@gmail.com>
2026-01-14 16:27:27 +0100

    Update GarderobeService.java

--- BACKEND/src/main/java/BACKEND/Service/GarderobeService.java
+++ BACKEND/src/main/java/BACKEND/Service/GarderobeService.java
@@ -1,50 +1,44 @@
 package BACKEND.Service;
 
 import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import BACKEND.Model.Hose;
+import BACKEND.Model.Outfit;
+import BACKEND.Model.Schuhe;
+import BACKEND.Model.Tshirt;
+import BACKEND.Repository.KleidungsstueckRepository;
+import BACKEND.Repository.OutfitRepository;
 
 @Service
 public class GarderobeService {
+    // Zugriff auf gespeicherte Outfits und auf Tshirts / Hosen / Schuhe
+    private final OutfitRepository outfitRepository;
+    private final KleidungsstueckRepository kleidungsstueckRepository;
 
-    public List<String> getTshirts() {
-        return List.of(
-                "assets/images/t-shirt1.jpg",
-                "assets/images/t-shirt1.jpg",
-                "assets/images/t-shirt2.jpg",
-                "assets/images/t-shirt4.jpg"
-        );
+    // // Spring gibt hier automatisch die richtigen Repositories rein
+    public GarderobeService(
+            OutfitRepository outfitRepository,
+            KleidungsstueckRepository kleidungsstueckRepository) {
+        this.outfitRepository = outfitRepository;
+        this.kleidungsstueckRepository = kleidungsstueckRepository;
     }
 
-    public List<String> getHosen() {
-        return List.of(
-                "assets/images/hose4.jpg",
-                "assets/images/hose2.jpg",
-                "assets/images/hose3.jpg",
-                "assets/images/hose4.jpg"
-        );
+    // Holt alle Outfits aus dem Repository (die im Creator erstellt wurden)
+    public List<Outfit> getOutfits() {
+        return outfitRepository.alleOutfitsListe();
+    }
+    // Holt alle Tshirts aus der Datenbank
+    public List<Tshirt> getTshirts() {
+        return kleidungsstueckRepository.alleTshirtsListe();
+    }
+    
+    public List<Hose> getHosen() {
+        return kleidungsstueckRepository.alleHosenListe();
     }
 
-    public List<String> getSchuhe() {
-        return List.of(
-                "assets/images/shuhe1.jpg",
-                "assets/images/shuhe2.jpg",
-                "assets/images/shuhe4.jpg",
-                "assets/images/shuhe4.jpg"
-        );
-    }
-
-    public List<List<String>> getOutfits() {
-        return List.of(
-                List.of(
-                        "assets/images/t-shirt1.jpg",
-                        "assets/images/hose2.jpg",
-                        "assets/images/shuhe4.jpg"
-                ),
-                List.of(
-                        "assets/images/t-shirt2.jpg",
-                        "assets/images/hose1.jpg",
-                        "assets/images/shuhe4.jpg"
-                )
-        );
+    public List<Schuhe> getSchuhe() {
+        return kleidungsstueckRepository.alleSchuheListe();
     }
 }
